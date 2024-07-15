18a34b1f982bb43f71d07fe220298f536cb90ec6
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2024-07-15 09:39:49 +0530

    Two sum 🚀

---
+++ two_sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int more = target - nums[i];
+
+            if (m.find(more) != m.end())
+            {
+                return {m[more], i};
+            }
+
+            m[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
